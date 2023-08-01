5f74b3017befe221a498f22c6d36745214e66a99
Yuki_w6 <m1610600@gmail.com>
2023-08-01 22:24:35 +0900

    rust 3

---
+++ hello_cargo/target/debug/deps/hello_cargo-f0f354d2afc29737.d
@@ -0,0 +1,5 @@
+/Users/mayamayuki/projects/rust_study/hello_cargo/target/debug/deps/hello_cargo-f0f354d2afc29737.rmeta: src/main.rs
+
+/Users/mayamayuki/projects/rust_study/hello_cargo/target/debug/deps/hello_cargo-f0f354d2afc29737.d: src/main.rs
+
+src/main.rs:
