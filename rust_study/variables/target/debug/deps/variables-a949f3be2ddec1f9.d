15fc1fa7ac44139c7136ffe711e1fad0e0bd2dcd
yuki-w6 <m1610600@gmail.com>
2024-01-08 17:36:57 +0900

    rust study section3

---
+++ rust_study/variables/target/debug/deps/variables-a949f3be2ddec1f9.d
@@ -0,0 +1,5 @@
+/home/mayamayk/projects/rust_study/rust_study/rust_study/variables/target/debug/deps/variables-a949f3be2ddec1f9: src/main.rs
+
+/home/mayamayk/projects/rust_study/rust_study/rust_study/variables/target/debug/deps/variables-a949f3be2ddec1f9.d: src/main.rs
+
+src/main.rs:
---
+++ rust_study/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/mayamayk/projects/rust_study/rust_study/rust_study/variables/target/debug/variables: /home/mayamayk/projects/rust_study/rust_study/rust_study/variables/src/main.rs
