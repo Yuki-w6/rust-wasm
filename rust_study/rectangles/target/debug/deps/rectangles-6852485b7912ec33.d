f904b3b5923ac8ea9b7d5b6871ac68ff67d5779b
yuki-w6 <m1610600@gmail.com>
2024-01-09 23:19:58 +0900

    rust study section 6

---
+++ rust_study/rectangles/target/debug/deps/rectangles-6852485b7912ec33.d
@@ -0,0 +1,5 @@
+/home/mayamayk/projects/rust_study/rust_study/rust_study/rectangles/target/debug/deps/rectangles-6852485b7912ec33: src/main.rs
+
+/home/mayamayk/projects/rust_study/rust_study/rust_study/rectangles/target/debug/deps/rectangles-6852485b7912ec33.d: src/main.rs
+
+src/main.rs:
---
+++ rust_study/rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/mayamayk/projects/rust_study/rust_study/rust_study/rectangles/target/debug/rectangles: /home/mayamayk/projects/rust_study/rust_study/rust_study/rectangles/src/main.rs
